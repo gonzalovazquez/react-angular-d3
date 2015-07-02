caaa997967efdcd131c552db98e2a58d9bfe834c
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-07-02 18:54:47 -0400

    fully functional angular in react

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/** @jsx React.DOM */
+var APP = 
+	React.createClass({displayName: 'APP',
+		componentWillReceiveProps: function(nextProps) {
+			console.log(nextProps);
+			if (nextProps.data) {
+				this.renderChart(nextProps.data)
+			}
+		},
+		renderChart: function(dataset) {
+			d3.select('#' + this.props.target).selectAll('div')
+			  .data(dataset)
+			  .enter()
+			  .append('div')
+			  .attr('class', 'bar')
+			  .style('height', function(d) {
+			  	return d.val * 8 + 'px';
+			  });
+		},
+		render: function() {
+			console.log('Hello from React', this.props);
+			return React.DOM.div({ id: this.props.target })
+		}
+	});
