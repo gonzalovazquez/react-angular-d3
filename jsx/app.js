b4e1d6d5d347319419b452617d138ab1e78ae2a1
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-07-02 17:30:49 -0400

    added d3js to react

--- jsx/app.js
+++ jsx/app.js
@@ -2,19 +2,30 @@
 var APP = 
 	React.createClass({
 		getInitialState: function() {
-			return {data: [
-					{val:9},
-					{val:3},
-					{val:4},
-					{val:2},
-					{val:5}
-				]}
+			return {data: []}
+		},
+		componentWillMount: function() {
+			reqwest({
+				url: 'http://filltext.com/?rows=10&val={number}',
+				type: 'jsonp',
+				success: function(resp) {
+					this.setState({ data:resp })
+					this.renderChart(this.state.data)
+				}.bind(this)
+			})
+		},
+		renderChart: function(dataset) {
+			d3.select('#chart').selectAll('div')
+			  .data(dataset)
+			  .enter()
+			  .append('div')
+			  .attr('class', 'bar')
+			  .style('height', function(d) {
+			  	return d.val * 8 + 'px';
+			  });
 		},
 		render: function() {
-			var items = this.state.data.map(function(item){
-				return <li>{ item.val }</li>
-			})
-			return <ul>{ items }</ul>
+			return <div id="chart"></div>
 		}
 	});
 React.render(
